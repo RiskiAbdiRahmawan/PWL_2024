71266042ffaebbc4aa8d6fb6907afc743977175d
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2024-02-20 10:20:29 +0700

    membuat route article dengan parameter id

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::get('/user/{name}', function ($name) {
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . ' Komentar ke-' . $commentId;
 });
+Route::get('/articles/{id}',function($articleId){
+    return 'Halaman artikel dengan ID ' . $articleId;
+});
