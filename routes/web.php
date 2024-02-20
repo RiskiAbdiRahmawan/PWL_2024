ac8cfd04764ed8d5e2cf59fbc79699451d52535e
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2024-02-20 09:53:54 +0700

    membuat route halo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello World';
+});
+Route::get('/halo', function () {
+    return 'Hello World';
 });
