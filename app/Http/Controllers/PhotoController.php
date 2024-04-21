72cbb2ed5b590b91f111e689613c496c01c5a1a2
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2024-04-21 13:26:58 +0700

    Usercontroller

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -2,29 +2,208 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\m_level;
+use App\Models\m_user;
+use Yajra\DataTables\Contracts\DataTable;
+use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class PhotoController extends Controller
+class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Menampilkan halaman awal user
     public function index()
     {
-        //
+        $breadcrumb = (object)[
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+
+        $page = (object)[
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'user'; // set menu yang sedang aktif
+
+        $level = m_level::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
+        // Update data user dengan Eloquent model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // m_user::where('username', 'customer-1')->update($data);
+        // // //tambah data user dengan Eloquent model
+        // // $data = [
+        // //     'username' => 'customer-1',
+        // //     'nama' => 'Pelanggan',
+        // //     'password' => Hash::make('12345'),
+        // //     'level_id' => '5',
+        // // ];
+        // // m_user::insert($data); //tambahkan data ke tables m_users   
+        // // coba akses model UserModel
+        // $user = m_user::all(); // ambil semua data dari tabel m_users
+        // return view('user', ['data' => $user]);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // m_user::create($data);
+
+        // $user = m_user::all();
+        // return view('user',['data'=>$user]);
+
+        // $user = m_user::find(1);
+        // return view('user',['data'=>$user]);
+
+        // $user = m_user::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::firstwhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::findor(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+        // $user = m_user::findor(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::findorfail(1);
+        // return view('user', ['data' => $user]);
+        // $user = m_user::where('username', 'manager9')->firstorfail();
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::where('level_id', 2)->count();
+        // // dd($user);
+        // return view('user', ['data' => $user]);
+        // $user = m_user::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+        // $user = m_user::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::create(
+        //     [
+        //         'username' => 'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+        // $user->username = 'manager56';
+
+        // $user->isDirty();//true
+        // $user->isDirty('username');//true
+        // $user->isDirty('nama');//false
+        // $user->isDirty(['nama','username']);//true
+
+        // $user->isClean();//flase
+        // $user->isClean('username');//flase
+        // $user->isClean('nama');//true
+        // $user->isClean(['nama','username']);//flase
+
+        // $user->save();
+
+        // $user->isDirty();//false
+        // $user->isClean();//true
+        // dd($user->isDirty());
+
+        // $user = m_user::create(
+        //     [
+        //         'username' => 'manager11',
+        //         'nama' => 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ],
+        // );
+
+        // $user->username = 'manager12';
+
+        // $user->save();
+
+        // $user->wasChanged();//true
+        // $user->wasChanged('username');//true
+        // $user->wasChanged(['username','level_id']);//true
+        // $user->wasChanged('nama');//flase
+        // dd($user->wasChanged(['nama','username']));//true
+
+        // $user = m_user::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = m_user::with('level')->get();
+        // return view('user', ['data' => $user]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    // Ambil data user dalam bentuk json untuk datatables
+    public function list(Request $request)
+    {
+        $users = m_user::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+        return DataTables::of($users)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($user) {
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a>';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a>';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">' . csrf_field() . method_field('DELETE') . '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
     public function create()
     {
-        //
+        $breadcrumb = (object)[
+            'title' => 'Tambah User',
+            'list' => ['Home', 'User', 'Tambah']
+        ];
+        $page = (object)[
+            'title' => 'Tambah User Baru'
+        ];
+
+        $level = m_level::all(); //ambil data untuk ditampilkan di form
+        $activeMenu = 'user';
+        return view('user.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -45,35 +224,128 @@ class PhotoController extends Controller
         return redirect('/user')->with('success', 'Data user berhasil disimpan');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $user = m_user::with('level')->find($id);
+
+        $breadcrumb = (object)[
+            'title' => 'Detail User',
+            'list' => ['Home', 'User', 'Detail']
+        ];
+        $page = (object)[
+            'title' => 'Detail User'
+        ];
+
+        $activeMenu = 'user';
+
+        return view('user.show', ['user' => $user, 'breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $user = m_user::find($id);
+        $level = m_level::all();
+
+        $breadcrumb = (object)[
+            'title' => 'Edit User',
+            'list' => ['Home', 'User', 'Edit']
+        ];
+        $page = (object)[
+            'title' => 'Edit User'
+        ];
+
+        $activeMenu = 'user';
+
+        return view('user.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'user' => $user, 'level' => $level]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_users,username,' . $id . ',user_id',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:5',
+            'level_id' => 'required|integer',
+        ]);
+
+        m_user::find($id)->update([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => $request->password ? bcrypt($request->password) : m_user::find($id)->password,
+            'level_id' => $request->level_id,
+        ]);
+
+        return redirect('/user')->with('success', 'Data user berhasil diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $check = m_user::find($id);
+        if (!$check) {
+            return redirect('/user')->with('error', 'Data user tidak ditemukan');
+        }
+
+        try {
+            m_user::destroy($id);
+
+            return redirect('/user')->with('success', 'Data user berhasil dihapus');
+        } catch (\illuminate\Database\QueryException $e) {
+            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
     }
+    // public function tambah()
+    // {
+    //     return view('user_tambah');
+    // }
+
+    // public function tambah_simpan(Request $request)
+    // {
+    //     $validated = $request->validate([
+    //         'username' => 'bail|required',
+    //         'nama' => 'required',
+    //         'password' => 'required',
+    //         'level_id' => 'required',
+    //     ]);
+    //     m_user::create([
+    //         'username' => $request->username,
+    //         'nama' => $request->nama,
+    //         'password' => Hash::make($request->password),
+    //         'level_id' => $request->level_id,
+    //     ]);
+    //     return redirect('/user');
+    // }
+
+    // public function ubah($id)
+    // {
+    //     $user = m_user::find($id);
+    //     return view('user_ubah', ['data' => $user]);
+    // }
+
+    // public function ubah_simpan($id, Request $request)
+    // {
+    //     $user = m_user::find($id);
+
+    //     $user->username = $request->username;
+    //     $user->nama = $request->nama;
+    //     $user->level_id = $request->level_id;
+
+    //     $user->save();
+    //     return redirect('/user');
+    // }
+
+    // public function hapus($id)
+    // {
+    //     $user = m_user::find($id);
+    //     $user->delete();
+
+    //     return redirect('/user');
+    // }
+    // public function create()
+    // {
+    //     return view('user.create');
+    // }
+    // public function update()
+    // {
+    //     return view('user.edit');
+    // }
 }
